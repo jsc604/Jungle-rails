b660877399afdf703307d1ab311598f253fe9a35
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-31 14:48:59 -0800

    implemented cypress test for add to cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,26 @@
+describe('Junge app', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('visits the Home Page', () => {
+    cy.contains('h1', 'The Jungle')
+  });
+
+  it('There are profucts on the page', () => {
+    cy.get('.products article').should('be.visible');
+  });
+  
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it('visits the product detail page of the product that was clicked and adds to cart', () => {
+    cy.get('[alt="Scented Blade"]').click()
+    cy.contains('.product-detail div h1', 'Scented Blade')
+    cy.get('.nav-item.end-0 .nav-link').contains('My Cart (0)')
+    cy.get('form').click()
+    cy.get('.nav-item.end-0 .nav-link').contains('My Cart (1)')
+  });
+
+});
