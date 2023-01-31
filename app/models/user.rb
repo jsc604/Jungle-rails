3c777a26f0f051a4f866566e9b3a2fcc3bebfe1f
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-30 17:47:56 -0800

    completed validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
+  validates :first_name, :last_name, :email, presence: true
 
 end
