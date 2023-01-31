ba68bfd54b3249c5accad50ce396d80bfccd9280
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-31 14:23:01 -0800

    implemented product details test on cypress

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,22 @@
+describe('Junge app', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('visits the Home Page', () => {
+    cy.contains('h1', 'The Jungle')
+  });
+
+  it('There are profucts on the page', () => {
+    cy.get('.products article').should('be.visible');
+  });
+  
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it('visits the product detail page of the product that was clicked', () => {
+    cy.get('[alt="Giant Tea"]').click()
+    cy.contains('.product-detail div h1', 'Giant Tea')
+  })
+})
